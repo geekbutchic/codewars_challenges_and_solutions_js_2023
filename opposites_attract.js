fc73a0975373e5057dd2fe1bfd43d4f362c452b0
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2023-10-02 16:13:05 -0600

    adds final solution

--- opposites_attract.js
+++ opposites_attract.js
@@ -22,6 +22,12 @@ function loveFunc(flower1, flower2) {
   return flower1 % 2 === 0 !== (flower2 % 2 === 0);
 }
 
+/* 
+1. (flower1 % 2 === 0) checks if flower1 has an even number of petals.
+2. (flower2 % 2 === 0) checks if flower2 has an even number of petals.
+The expression (flower1 % 2 === 0) !== (flower2 % 2 === 0) returns true if one of the flowers has an even number of petals and the other has an odd number, and false if they are both either even or odd. This simplifies the logic and reduces the number of if statements.
+*/
+
 console.log(loveFunc(1, 4));
 console.log(loveFunc(2, 2));
 console.log(loveFunc(0, 1));
