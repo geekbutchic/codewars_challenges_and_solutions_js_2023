1479c6d194a3c286c38ece614762e1e727bf0566
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-02-14 16:52:49 -0700

    solution to return strings

---
+++ return_strings.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?"
+
+//Solution
+const greet = n => `Hello, ${n} how are you doing today?`
+
+console.log(greet(`Sonny`));
