ee570e9ec738a2a2824af2b9027c90b72481266c
geekbutchic <71285487+geekbutchic@users.noreply.github.com>
2024-02-26 18:40:38 -0700

    small update

--- return_strings.js
+++ return_strings.js
@@ -2,7 +2,7 @@
 
 //Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?"
 
-//Solution
+//Refactored Solution
 const greet = n => `Hello, ${n} how are you doing today?`
 
 console.log(greet(`Sonny`));
